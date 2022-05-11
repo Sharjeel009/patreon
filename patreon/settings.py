3f2976600e220806776851c0014ee74621933f41
Sharjeel Ahmed <shrgil@gmail.com>
2022-05-11 15:38:06 +0500

    update creator app

--- patreon/settings.py
+++ patreon/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'creator',
+    # 'creator',
     'user.apps.UserConfig',
     'post.apps.PostConfig',
     'home.apps.HomeConfig'
