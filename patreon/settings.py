c51bf258e02e9be0191e21f2a4c7ed2693a32529
Sharjeel Ahmed <shrgil@gmail.com>
2022-05-11 14:41:06 +0500

    update config

--- patreon/settings.py
+++ patreon/settings.py
@@ -83,26 +83,28 @@ WSGI_APPLICATION = 'patreon.wsgi.application'
 
 if DEBUG:
 
-    DATABASES = {
-
-        
-        'default': {
-            'ENGINE': 'django.db.backends.mysql', 
-            'NAME': 'DB_PATREON',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-            'PORT': '3306',
-        }
-
-    }
-else:
     DATABASES={
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 
+
+    }
+
+  
+else:
+     DATABASES = {
+
+        
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', 
+            'NAME': 'DB_PATREON',
+            'USER': 'patronuser',
+            'PASSWORD': 'patronpa33word',
+            'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+            'PORT': '3306',
+        }
 
     }
 
@@ -132,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+5'
 
 USE_I18N = True
 
@@ -144,14 +146,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# static files storage
+
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'env_static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
